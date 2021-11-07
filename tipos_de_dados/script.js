5305bad8da511a0022fc9091692a32a608ff618f
aninhacerqueira <anaclaudiacribeiro27@gmail.com>
2021-11-07 16:54:30 -0300

    adicionando pastas de exercicios javascript

---
+++ tipos_de_dados/script.js
@@ -0,0 +1,38 @@
+// Declarar uma variável  contendo uma string
+
+let nome = "aninha ";
+
+console.log(nome);
+
+// Declarar uma variável  contendo um número dentro de uma string
+
+let anos = "21 anos";
+
+console.log(anos);
+
+// Declarar uma variável  com a sua idade
+
+let idade = 21;
+
+console.log(idade);
+
+// Declare duas variáveis, uma com o seu nome
+// e a outra com seu sobrenome e somes as mesmas
+
+let sobrenome = "ribeiro";
+
+let nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+
+let time = "It's time";
+
+console.log(time);
+
+// Verifique o tipo da variável que contém o seu nome
+
+let color = "orange and black";
+
+console.log(typeof color);
---
+++ variaveis/script.js
@@ -0,0 +1,29 @@
+// Declarar uma variável  com o seu nome
+var nome = "ana cláudia";
+console.log(nome);
+
+// Declarar uma Variável  com o seu idade
+
+var idade = "21 anos";
+console.log(idade);
+
+// Declarar uma variável com o sua comida
+// favorita e não atribuir valor
+
+var pizza;  
+console.log(pizza);
+
+// Atribuir valor a sua comida favorita
+
+var pizza = "23,00"
+console.log(pizza);
+
+// Declarar 5 Variáveis diferentes sem valores
+
+var sobrenome;
+var endereco;
+var escola;
+var musica; 
+var local;
+
+console.log(sobrenome, endereco, escola, musica, local);
